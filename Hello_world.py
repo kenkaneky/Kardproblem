2915a63912de55f031f1d3e1c2adfd560d4a10af
kenkaneky <azin.semen@gmail.com>
2025-09-22 23:02:06 +0400

    написал код для красивых чиселок

--- Hello_world.py
+++ Hello_world.py
@@ -1,1 +1,11 @@
-print("Hello world")
+kol_vo_chisel = int(input())
+if kol_vo_chisel % 2 == 0:
+    for i in range(1, kol_vo_chisel // 2 + 1):
+        print(i, end=" ")
+    for i in range(kol_vo_chisel // 2, 0, -1):
+        print(i, end=" ")
+else:
+    for i in range(1, kol_vo_chisel // 2 + 2):
+        print(i, end=" ")
+    for i in range(kol_vo_chisel // 2, 0, -1):
+        print(i, end=" ")
