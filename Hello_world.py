b10d5859887ca5124f0c6d6ed34ef82dd97aec4b
kenkaneky <87109234+kenkaneky@users.noreply.github.com>
2025-09-23 13:07:16 +0400

    Update Hello_world.py
    Вроде так

--- Hello_world.py
+++ Hello_world.py
@@ -1,11 +1,12 @@
 kol_vo_chisel = int(input())
-if kol_vo_chisel % 2 == 0:
-    for i in range(1, kol_vo_chisel // 2 + 1):
-        print(i, end=" ")
-    for i in range(kol_vo_chisel // 2, 0, -1):
-        print(i, end=" ")
-else:
-    for i in range(1, kol_vo_chisel // 2 + 2):
-        print(i, end=" ")
-    for i in range(kol_vo_chisel // 2, 0, -1):
-        print(i, end=" ")
+def chislki(kol_vo_chisel)
+    if kol_vo_chisel % 2 == 0:
+        for i in range(1, kol_vo_chisel // 2 + 1):
+            print(i, end=" ")
+        for i in range(kol_vo_chisel // 2, 0, -1):
+            print(i, end=" ")
+    else:
+        for i in range(1, kol_vo_chisel // 2 + 2):
+            print(i, end=" ")
+        for i in range(kol_vo_chisel // 2, 0, -1):
+            print(i, end=" ")
