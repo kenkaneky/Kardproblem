34fd44c018df14db70fd74c4b6e2f9add7014f11
kenkaneky <87109234+kenkaneky@users.noreply.github.com>
2025-09-23 18:38:56 +0400

    Update Hello_world.py
    вспомнил как работать с функциями, исправил

--- Hello_world.py
+++ Hello_world.py
@@ -1,5 +1,4 @@
-kol_vo_chisel = int(input())
-def chislki(kol_vo_chisel)
+def chislki(kol_vo_chisel):
     if kol_vo_chisel % 2 == 0:
         for i in range(1, kol_vo_chisel // 2 + 1):
             print(i, end=" ")
@@ -10,3 +9,4 @@ def chislki(kol_vo_chisel)
             print(i, end=" ")
         for i in range(kol_vo_chisel // 2, 0, -1):
             print(i, end=" ")
+primer = chislki(int(input()))
